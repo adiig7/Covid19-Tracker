8a467e4c634b02c0f3c894bbed7470b82cc60b55
adiig7 <54351909+adiig7@users.noreply.github.com>
2020-08-14 22:48:45 +0530

    api

---
+++ app/src/main/java/com/example/covidtracker/Client.kt
@@ -0,0 +1,14 @@
+package com.example.covidtracker
+
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+object Client {
+    private val client = OkHttpClient()
+
+    private val request = Request.Builder()
+        .url("https://api.covid19india.org/data.json")
+        .build()
+
+    val api = client.newCall(request)
+}
