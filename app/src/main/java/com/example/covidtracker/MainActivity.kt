96ec6a3f3c6fda85b002071332932ba127333cb2
adiig7 <54351909+adiig7@users.noreply.github.com>
2020-08-15 00:00:07 +0530

    api

--- app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.covidtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchResults()
+    }
+
+    private fun fetchResults() {
+        GlobalScope.launch {
+            val response = withContext(Dispatchers.IO) { Client.api.execute() }
+            if (response.isSuccessful){
+                val data = Gson().fromJson(response.body?.string(), Response::class.java)
+                launch(Dispatchers.Main) {
+                    bindCombinedData(data.statewise[0])
+                }
+            }
+        }
+    }
+
+    private fun bindCombinedData(get: StatewiseItem?) {
+
     }
 }
